f33b22cde2bf96b4858938bf6f708abf8cb94310
HossyH <hoshina@style-storage.com>
2016-05-28 03:41:21 +0900

    GetToken()

--- MypocketServerLink.go
+++ MypocketServerLink.go
@@ -21,43 +21,42 @@ import "net/http"
 import "io/ioutil"
 import "encoding/json"
 import "fmt"
-import "log"
 
 /*=====================================================================
-#     (failOnError)
+#     (Struct)
 #=====================================================================*/
-func failOnError(err error) {
-	if err != nil {
-		log.Fatal("Error:", err)
-	}
+type Token struct {
+	Issued string `json:"issued"`
+	Token  string `json:"token"`
 }
 
 /*=====================================================================
-#     (HTTP Request関数)
+#     (GetToken関数)
 #=====================================================================*/
-func HttpRequest() {
-	type Token struct {
-		issued string
-		token  string
-	}
-
+func GetToken() string {
 	req, _ := http.NewRequest("GET", "https://cocoa.ntt.com/rest/users/v1/token?mode=0", nil)
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
 	req.Header.Set("X-WSSE", "UsernameToken Username=\"hoshina@trust.ocn.ne.jp\",PasswordText=\"Kaei8349aA8k1\",AccessKey=\"Mypocket58316053905d254dd29c6ed97ba53caa471f3ce977db9a1fbc3af9cd2955614d6ad9cf4499fee5c694c0a54e91fd25ae7f17b442631605270001\",UsernameType=\"1\"")
-
 	client := new(http.Client)
 	resp, _ := client.Do(req)
 	defer resp.Body.Close()
 
-	byteArray, _ := ioutil.ReadAll(resp.Body)
+	byteArray, err := ioutil.ReadAll(resp.Body)
 
-	var js Token
-	json.Unmarshal([]byte(string(byteArray)), &js)
-	fmt.Println(js)
+	var output Token
+
+	err = json.Unmarshal(byteArray, &output)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return string(output.Token)
 }
 
 /*=====================================================================
 #     (main関数)
 #=====================================================================*/
 func main() {
-	HttpRequest()
+	fmt.Printf(GetToken())
 }
