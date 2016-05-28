0ab995024cd041c6067f7a0fd40e1394a053936e
HossyH <hoshina@style-storage.com>
2016-05-28 09:45:15 +0900

    GetToken

--- MypocketServerLink.go
+++ MypocketServerLink.go
@@ -31,12 +31,19 @@ type Token struct {
 }
 
 /*=====================================================================
+#     (Global定数)
+#=====================================================================*/
+var MyPocket_Email = "hoshina@trust.ocn.ne.jp"
+var MyPocket_Password = "Kaei8349aA8k1"
+var MyPocket_AccessKey = "Mypocket58316053905d254dd29c6ed97ba53caa471f3ce977db9a1fbc3af9cd2955614d6ad9cf4499fee5c694c0a54e91fd25ae7f17b442631605270001"
+
+/*=====================================================================
 #     (GetToken関数)
 #=====================================================================*/
-func GetToken() string {
+func GetToken(X_WSSE_DATA string) string {
 	req, _ := http.NewRequest("GET", "https://cocoa.ntt.com/rest/users/v1/token?mode=0", nil)
 	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	req.Header.Set("X-WSSE", "UsernameToken Username=\"hoshina@trust.ocn.ne.jp\",PasswordText=\"Kaei8349aA8k1\",AccessKey=\"Mypocket58316053905d254dd29c6ed97ba53caa471f3ce977db9a1fbc3af9cd2955614d6ad9cf4499fee5c694c0a54e91fd25ae7f17b442631605270001\",UsernameType=\"1\"")
+	req.Header.Set("X-WSSE", X_WSSE_DATA)
 	client := new(http.Client)
 	resp, _ := client.Do(req)
 	defer resp.Body.Close()
@@ -55,8 +62,29 @@ func GetToken() string {
 }
 
 /*=====================================================================
+#     (FileCopy関数)
+#=====================================================================*/
+func FileCopy() {
+	req, _ := http.NewRequest("POST", "https://cocoa.ntt.com/rest/storage/v1/files/copy", nil)
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Content-length", "68")
+	req.Header.Set("Authorization", "")
+
+}
+
+/*=====================================================================
 #     (main関数)
 #=====================================================================*/
 func main() {
-	fmt.Printf(GetToken())
+
+	var X_WSSE_DATA string
+	X_WSSE_DATA += "UsernameToken Username=\""
+	X_WSSE_DATA += MyPocket_Email
+	X_WSSE_DATA += "\",PasswordText=\""
+	X_WSSE_DATA += MyPocket_Password
+	X_WSSE_DATA += "\",AccessKey=\""
+	X_WSSE_DATA += MyPocket_AccessKey
+	X_WSSE_DATA += "\",UsernameType=\"1\""
+	fmt.Println(X_WSSE_DATA)
+	fmt.Printf(GetToken(X_WSSE_DATA))
 }
